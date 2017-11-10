c548c1d4d93c3d8c3d1d001d7eb190d56755da14
AJEvbank <austin.eubank@ttu.edu>
2017-11-10 08:57:20 -0600

    Install script and build script test.
    I tested the build script again and I think I've completed the install script.

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-BASE_URL=https://download.unity3d.com/download_unity/
+BASE_URL=https://download.unity3d.com/download_unity
 HASH=5d30cf096e79
-VERSION=5.4.1f1
+VERSION=2017.1.1f1
 
 download() {
   file=$1
@@ -17,7 +17,7 @@ install() {
   download "$package"
 
   echo "Installing "`basename "$package"`
-  sudo installer -dumplog -package `basename "$package"` -target /
+  sudo installer -dumplog -package `basename "$package"` -target -verbose /
 }
 
 # See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
@@ -25,5 +25,4 @@ install() {
 
 install "MacEditorInstaller/Unity-$VERSION.pkg"
 install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
-install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
-install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-$VERSION.pkg"
